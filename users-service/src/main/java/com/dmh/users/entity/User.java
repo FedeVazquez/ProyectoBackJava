14043a713cdc610f54ef6231fedfa6cc52652413
Federico_Vazquez <fvazquez@softguard.com>
2026-09-17 16:53:22 -0300

    entidad y repository para users

---
+++ users-service/src/main/java/com/dmh/users/entity/User.java
@@ -0,0 +1,44 @@
+package com.dmh.users.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	@Column(nullable = false, length = 50)
+	private String lastName;
+
+	@Column(nullable = false, unique = true, length = 20)
+	private String dni;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+
+	@Column(nullable = false, length = 20)
+	private String phoneNumber;
+
+	@Column(nullable = false)
+	private String password;
+
+}
---
+++ users-service/src/main/java/com/dmh/users/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.dmh.users.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dmh.users.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+
+	boolean existsByEmail(String email);
+
+	boolean existsByDni(String dni);
+}
